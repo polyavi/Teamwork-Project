4b0c41c0762196ed2c364935fb48defa9a43844e
Polya Ivanova <polia.v.ivanova@gmail.com>
2017-01-01 19:26:05 +0200

    projects in home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-import { ViewProjectsComponent } from '../projects';
+
+import { ProjectsService } from './../../core/services/projects.service';
+import { Project } from './../../core/models/project';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [ ProjectsService ]
 })
 export class HomeComponent implements OnInit {
+  public projects: Project[];
 
-  constructor() { }
+  constructor(private projectsService: ProjectsService) { }
 
   ngOnInit() {
+      this.projectsService.getAll()
+              .subscribe(projects => {
+                  this.projects = projects;
+                  console.log(projects);
+              });
   }
 
 }
