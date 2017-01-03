dc25582b5d041644e52d105bbde9595d7f4fb750
Polya Ivanova <polia.v.ivanova@gmail.com>
2017-01-03 07:22:46 +0200

    fix

--- src/data/events.ts
+++ src/data/events.ts
@@ -17,13 +17,4 @@ export const EVENTS: any[] = [{
     purpose: 'Event purpose',
     owner_id: 1,
     image_url: 'http://www.rtoronto.ca/wp-content/uploads/2015/03/Best-Clubs-in-Toronto.jpg'
-},
-{
-    id: 3,
-    title: 'le tour de France',
-    type: 'Cycling',
-    when: '2017-07-01',
-    where: 'Dusseldorf',
-    purpose: 'Event purpose',
-    image_url: 'https://bettingsite.com.au/wp-content/uploads/2016/08/Le-Tour-de-France.png'
 }];
