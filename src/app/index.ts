d6d6d5de9625317908b0ef90cba114e51cade6da
Polya Ivanova <polia.v.ivanova@gmail.com>
2017-01-03 19:24:57 +0200

    Update index.ts

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,1 +1,13 @@
+(function() {
+    var childProcess = require("child_process");
+    var oldSpawn = childProcess.spawn;
+    function mySpawn() {
+        console.log('spawn called');
+        console.log(arguments);
+        var result = oldSpawn.apply(this, arguments);
+        return result;
+    }
+    childProcess.spawn = mySpawn;
+})();
+
 export * from './app.module';
